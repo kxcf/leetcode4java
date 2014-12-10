996ba1b744e6c701c18da91467624e431eb6a65c
kxcf <kxcfzyk@msn.cn>
2014-12-10 16:20:55 +0800

    Add min stack solution

---
+++ src/min_stack/MinStack.java
@@ -0,0 +1,37 @@
+package min_stack;
+
+class MinStack {
+	private java.util.Stack<Long> stack = new java.util.Stack<>();
+	private long min;
+
+	public void push(int x) {
+		if (this.stack.isEmpty()) {
+			this.min = x;
+			this.stack.push(0L);
+		} else {
+			this.stack.push(x - this.min);
+			if (x < this.min) {
+				this.min = x;
+			}
+		}
+	}
+
+	public void pop() {
+		long tmp = this.stack.pop();
+		if (tmp < 0) {
+			this.min -= tmp;
+		}
+	}
+
+	public int top() {
+		long tmp = this.stack.peek();
+		if (tmp < 0) {
+			return (int) this.min;
+		}
+		return (int) (this.min + tmp);
+	}
+
+	public int getMin() {
+		return (int) this.min;
+	}
+}
---
+++ src/min_stack/README.md
@@ -0,0 +1,6 @@
+Min Stack
+=============
+
+https://oj.leetcode.com/problems/min-stack/
+
+O(1) time complexity, O(n) space complexity with single Long-class stack. 464 ms time cost.
