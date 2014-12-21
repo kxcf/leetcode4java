66982a92686b0a848725a92d676fcc5f3329e991
kxcf <kxcfzyk@msn.cn>
2014-12-21 18:17:03 +0800

    add word break solution

---
+++ src/word_break/README.md
@@ -0,0 +1,8 @@
+Word Break
+==========
+
+https://oj.leetcode.com/problems/word-break/
+
+O(n^2) time complexity, O(n) space complexity.
+
+DP solution, 380 ms time cost.
---
+++ src/word_break/Solution.java
@@ -0,0 +1,31 @@
+package word_break;
+
+import java.util.*;
+
+public class Solution {
+	public boolean wordBreak(String s, Set<String> dict) {
+		//breakable[i] means whether s.substring(0, i) can break or not.
+		boolean[] breakable = new boolean[s.length() + 1];
+
+		//suppose an empty string is always breakable.
+		breakable[0] = true;
+
+		//determine whether s.substring(0, len) can break or not.
+		//s.substring(0, len) is breakable if exist i in range [0, len) that
+		// s.substring(0, i) can break and dict contains s.substring(i)
+		for (int len = 1; len <= s.length(); len++) {
+			for (int i = 0; !breakable[len] && i < len; i++) {
+				breakable[len] = breakable[i] && dict.contains(s.substring(i, len));
+			}
+		}
+
+		return breakable[s.length()];
+	}
+
+	public static void main(String[] args) {
+		Set<String> dict = new HashSet<>();
+		dict.add("leet");
+		dict.add("code");
+		System.out.println(new Solution().wordBreak("leetcodes", dict));
+	}
+}
