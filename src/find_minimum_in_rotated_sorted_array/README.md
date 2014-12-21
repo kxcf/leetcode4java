1dd56a1eb17a650673b7680c11c2fc6e415b8888
kxcf <kxcfzyk@msn.cn>
2014-12-21 22:47:38 +0800

    add find minimum in rotated sorted array solution

---
+++ src/find_minimum_in_rotated_sorted_array/README.md
@@ -0,0 +1,8 @@
+Find Minimum in Rotated Sorted Array
+======================================
+
+https://oj.leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+
+O(log n) time complexity, O(1) space complexity.
+
+Binary search, 404 ms time cost.
---
+++ src/find_minimum_in_rotated_sorted_array/Solution.java
@@ -0,0 +1,21 @@
+package find_minimum_in_rotated_sorted_array;
+
+public class Solution {
+	public int findMin(int[] num) {
+		int left = 0, right = num.length - 1;
+		while (left < right) {
+			int mid = (left + right) >> 1;
+			//left<=mid<right
+			if (num[mid] > num[right]) {
+				left = mid + 1;
+			} else {
+				right = mid;
+			}
+		}
+		return num[left];
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new Solution().findMin(new int[]{4, 5, 6, 7}));
+	}
+}
