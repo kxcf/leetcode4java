a0e43b98e398619dcf1f657624a9f0923502230b
kxcf <kxcfzyk@msn.cn>
2014-12-21 20:48:27 +0800

    a few optimization of word break solution

--- src/word_break/Solution.java
+++ src/word_break/Solution.java
@@ -4,8 +4,10 @@ import java.util.*;
 
 public class Solution {
 	public boolean wordBreak(String s, Set<String> dict) {
+		int length = s.length();
+
 		//breakable[i] means whether s.substring(0, i) can break or not.
-		boolean[] breakable = new boolean[s.length() + 1];
+		boolean[] breakable = new boolean[length + 1];
 
 		//suppose an empty string is always breakable.
 		breakable[0] = true;
@@ -13,13 +15,13 @@ public class Solution {
 		//determine whether s.substring(0, len) can break or not.
 		//s.substring(0, len) is breakable if exist i in range [0, len) that
 		// s.substring(0, i) can break and dict contains s.substring(i)
-		for (int len = 1; len <= s.length(); len++) {
+		for (int len = 1; len <= length; len++) {
 			for (int i = 0; !breakable[len] && i < len; i++) {
 				breakable[len] = breakable[i] && dict.contains(s.substring(i, len));
 			}
 		}
 
-		return breakable[s.length()];
+		return breakable[length];
 	}
 
 	public static void main(String[] args) {
